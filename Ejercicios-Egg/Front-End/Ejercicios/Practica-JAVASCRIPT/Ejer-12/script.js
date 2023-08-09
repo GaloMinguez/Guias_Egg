ad8c3808be2178714d3f7bfa7c42bf7dd38825f7
GaloMinguez <127149679+GaloMinguez@users.noreply.github.com>
2023-08-09 20:22:15 -0300

    empezando Ejercicios

---
+++ Ejercicios-Egg/Front-End/Ejercicios/Practica-JAVASCRIPT/Ejer-12/script.js
@@ -0,0 +1,8 @@
+//Escribir una función flecha de JavaScript 
+//que reciba un argumento y retorne el tipo de dato.
+
+var valor = prompt("ingrese algo")
+
+let func = (valor) => console.log(typeof(valor));
+
+func(valor);
